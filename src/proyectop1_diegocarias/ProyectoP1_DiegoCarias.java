07e8c873af4731f0b7f14ba19a71d237e886ba06
Diego Carias <167444337+MrSak123@users.noreply.github.com>
2024-06-09 21:03:27 -0600

    commit 1

---
+++ src/proyectop1_diegocarias/ProyectoP1_DiegoCarias.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyectop1_diegocarias;
+
+/**
+ *
+ * @author djcar
+ */
+public class ProyectoP1_DiegoCarias {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
